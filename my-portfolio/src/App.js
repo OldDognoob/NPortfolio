5a3cfdb5e24149e4d04005a26404935f19a9ce71
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-06-23 18:47:37 +0300

    fix-app

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Home from "./components/Home";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import {useLocation} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route,useLocation} from "react-router-dom";
 import Header from "./components/Header/Header";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Contact from "./components/Contact/Contact";
@@ -12,22 +11,22 @@ function App() {
   let location = useLocation();
   // console.log(location);
   return (
-  <Router>
+  <div>
     <Header></Header>
     <AnimatePresence exitBeforeEnter initial={false}>
       <Switch location={location} key={location.pathname}>
       <Route exact path='/'>
             <Home></Home>
           </Route>
-        <Route path="/about/">
+        <Route path="/about">
           <AboutMe></AboutMe>
         </Route>
-        <Route path="/contact/">
+        <Route path="/contact">
           <Contact></Contact>
         </Route>
       </Switch>
     </AnimatePresence>
-    </Router>
+    </div>
   );
 }
 
