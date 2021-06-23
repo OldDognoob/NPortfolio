2628f090d2bdf79e096bf16153095eabd3067e20
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-06-23 18:38:48 +0300

    wip-project-configurations

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Home from "./components/Home";
-import {BrowserRouter as Router, Switch, Route, Link, NavLink, useLocation} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {useLocation} from "react-router-dom";
+import Header from "./components/Header/Header";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Contact from "./components/Contact/Contact";
 import { AnimatePresence } from "framer-motion";
@@ -8,17 +10,19 @@ import './App.css';
 
 function App() {
   let location = useLocation();
+  // console.log(location);
   return (
   <Router>
+    <Header></Header>
     <AnimatePresence exitBeforeEnter initial={false}>
       <Switch location={location} key={location.pathname}>
       <Route exact path='/'>
             <Home></Home>
           </Route>
-        <Route path="/about">
+        <Route path="/about/">
           <AboutMe></AboutMe>
         </Route>
-        <Route path="/contact">
+        <Route path="/contact/">
           <Contact></Contact>
         </Route>
       </Switch>
--- my-portfolio/src/components/Header/Header.js
+++ my-portfolio/src/components/Header/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faBookOpen,
   faEnvelope,
   faFileAlt,
   faHome,
