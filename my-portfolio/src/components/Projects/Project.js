93af16fa9672a95fb2af16eade1a1e85b3baf8e8
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-06-29 21:11:31 +0300

    adding-projects

---
+++ my-portfolio/src/components/Projects/Project.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Masonry from "react-masonry-css";
+
+/*Projects*/
+
+
+import {motion} from"framer-motion";
+import SingleProject from "./SingleProject";
+
+/*Project css */
+import "./Project.css";
+
