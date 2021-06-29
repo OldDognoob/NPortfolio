00ae481a2eece4f0a599f96f0cd787ee43b36b77
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-06-29 21:46:30 +0300

    wip-projects-file

--- my-portfolio/src/components/Projects/Project.js
+++ my-portfolio/src/components/Projects/Project.js
@@ -10,3 +10,83 @@ import SingleProject from "./SingleProject";
 /*Project css */
 import "./Project.css";
 
+const Projects = () => {
+    const projects =[
+        {
+            img:"",
+            title:"",
+            description:"",
+            technology:[],
+            livesite:"",
+            github:""
+        },
+        {
+            img:"",
+            title:"",
+            description:"",
+            technology:[],
+            livesite:"",
+            github:""
+        },
+        {
+            img:"",
+            title:"",
+            description:"",
+            technology:[],
+            livesite:"",
+            github:""
+        },
+        {
+            img:"",
+            title:"",
+            description:"",
+            technology:[],
+            livesite:"",
+            github:""
+        },
+        {
+            img:"",
+            title:"",
+            description:"",
+            technology:[],
+            livesite:"",
+            github:""
+        },
+        {
+            img:"",
+            title:"",
+            description:"",
+            technology:[],
+            livesite:"",
+            github:""
+        },
+    ];
+    const breakpointColumnsObj = {
+        default:3,
+        1100:2,
+        700:2,
+        500:1
+    };
+    return(
+        <motion.div
+        initial={{opacity:0, scale:0.5}}
+        animate={{opacity:1, scale:1}}
+        exit={{opacity:0, scale:0.5}}
+        transition={{duration:0.5}}
+        >
+            <div className="container">
+                <h2 className="brand-text mb-5">My Projects:</h2>
+                <Masonry
+                breakpointCols={breakpointColumnsObj}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+                >
+                    {projects.map((project)=>(
+                        <SingleProject project={project}></SingleProject>
+                    ))}
+                </Masonry>
+            </div>
+        </motion.div>
+    );
+};
+export default Projects;
