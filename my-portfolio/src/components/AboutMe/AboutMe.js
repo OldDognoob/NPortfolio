27583498cfda1a0f4b1016feb7e58ba98d2ff1bf
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-06-30 22:33:56 +0300

    wip-project

--- my-portfolio/src/components/AboutMe/AboutMe.js
+++ my-portfolio/src/components/AboutMe/AboutMe.js
@@ -8,26 +8,20 @@ import {
   faMobile,
 } from "@fortawesome/free-solid-svg-icons";
 
-import {skills} from "../Skills/skills-data";
-// import {skillsItem} from "../Skills/skillsItem";
-
-import { Row, Col, Container } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.css";
-
 const AboutMe = () => {
-  const frontendskills = [
-    "HTML5",
-    "JAVASCRIPT",
-    "CSS",
-    "REACT",
-    "REDUX"
+  // const frontendskills = [
+  //   "HTML5",
+  //   "JAVASCRIPT",
+  //   "CSS",
+  //   "REACT",
+  //   "REDUX"
    
-  ];
-  const backendskills = ["NODE.JS", "EXPRESS"];
-  const database = ["PostgreSQL", "MS-SQL", "SQL", "MongoDB"];
-  const programming = ["PYTHON"];
-  const version = ["GITHUB","GIT"];
-  const familiar = ["NEXT.JS","SASS", "LESS", "BOOTSTRAP","MATERIAL-UI","TAILWIND"];
+  // ];
+  // const backendskills = ["NODE.JS", "EXPRESS"];
+  // const database = ["PostgreSQL", "MS-SQL", "SQL", "MongoDB"];
+  // const programming = ["PYTHON"];
+  // const version = ["GITHUB","GIT"];
+  // const familiar = ["NEXT.JS","SASS", "LESS", "BOOTSTRAP","MATERIAL-UI","TAILWIND"];
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0.5 }}
@@ -35,7 +29,7 @@ const AboutMe = () => {
       exit={{ opacity: 0, scale: 0.5 }}
       transition={{ duration: 0.5 }}
     >
-      <div className="container pb-5">
+      <div div className="container pb-5">
         <div className="row">
           <h1 className="brand-text mb-5">About Me</h1>
           <div className="col-md-8">
@@ -68,7 +62,7 @@ const AboutMe = () => {
             </a>
           </div>
         </div>
-        <h3 className="text-center mt-5">My Skills Set</h3>
+        {/* <h3 className="text-center mt-5">My Skills Set</h3>
         <div className="row mt-5 text-center mb-5">
           <div className="col-md-4">
             <div className="service-box">
@@ -78,49 +72,48 @@ const AboutMe = () => {
               ))}
            
             </div>
-          </div>
-          <div className="col-md-4">
+          </div> */}
+          {/* <div className="col-md-4">
             <div className="service-box">
               <h4 className="brand-text">Backend skills: </h4>
               {backendskills.map((skill) => (
                 <span className="singleSkill">{skill}</span>
               ))}
             </div>
-          </div>
-          <div className="col-md-4">
+          </div> */}
+          {/* <div className="col-md-4">
             <div className="service-box">
               <h4 className="brand-text">Database skills: </h4>
               {database.map((skill) => (
                 <span className="singleSkill">{skill}</span>
               ))}
             </div>
-          </div>
-          <div className="col-md-4">
+          </div> */}
+          {/* <div className="col-md-4">
             <div className="service-box">
               <h4 className="brand-text">Programming Language: </h4>
               {programming.map((skill) => (
                 <span className="singleSkill">{skill}</span>
               ))}
             </div>
-          </div>
-          <div className="col-md-4">
+          </div> */}
+          {/* <div className="col-md-4">
             <div className="service-box">
               <h4 className="brand-text">Version Control: </h4>
               {version.map((skill) => (
                 <span className="singleSkill">{skill}</span>
               ))}
             </div>
-          </div>
-          <div className="col-md-4">
+          </div> */}
+          {/* <div className="col-md-4">
             <div className="service-box">
               <h4 className="brand-text">Familiar with: </h4>
               {familiar.map((skill) => (
                 <span className="lowskill">{skill}</span>
               ))}
             </div>
+          </div> */}
           </div>
-        </div>
-      </div>
     </motion.div>
   );
 };
