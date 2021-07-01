48f27e8825635e39307746f031a97e570e5731d1
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-07-01 20:45:57 +0300

    wip-project

--- my-portfolio/src/components/Skills/Skills.js
+++ my-portfolio/src/components/Skills/Skills.js
@@ -1,41 +1,20 @@
-
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "framer-motion";
-// import {
-//   faBootstrap,
-//   faHtml5,
-//   faCss3,
-//   faJs,
-//   faSass,
-//   faReact,
-//   faPython,
-//   faNodeJs,
-//   faExpress,
-//   faLess,
-// } from "@fortawesome/free-brands-svg-icons";
-// import { Row, Col, Container } from "react-bootstrap";
-import {
-  faDesktop,
-  faDownload,
-  faEye,
-  faMobile,
-} from "@fortawesome/free-solid-svg-icons";
 
 const Skills = () => {
-  // const frontendskills = [
-  //   "HTML5",
-  //   "JAVASCRIPT",
-  //   "CSS",
-  //   "REACT",
-  //   "REDUX"
-   
-  // ];
-  // const backendskills = ["NODE.JS", "EXPRESS"];
-  // const database = ["PostgreSQL", "MS-SQL", "SQL", "MongoDB"];
-  // const programming = ["PYTHON"];
-  // const version = ["GITHUB","GIT"];
-  // const familiar = ["NEXT.JS","SASS", "LESS", "BOOTSTRAP","MATERIAL-UI","TAILWIND"];
+  const frontendskills = [
+    "HTML5",
+    "JAVASCRIPT",
+    "CSS",
+    "REACT",
+    "REDUX"
+
+  ];
+  const backendskills = ["NODE.JS", "EXPRESS"];
+  const database = ["PostgreSQL", "MS-SQL", "SQL", "MongoDB"];
+  const programming = ["PYTHON"];
+  const version = ["GITHUB","GIT"];
+  const familiar = ["NEXT.JS","SASS", "LESS", "BOOTSTRAP","MATERIAL-UI","TAILWIND"];
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0.5 }}
@@ -43,7 +22,7 @@ const Skills = () => {
       exit={{ opacity: 0, scale: 0.5 }}
       transition={{ duration: 0.5 }}
     >
-      <div div className="container pb-5">
+      <div style={{ minHeight: "100vh" }} className="container pb-5">
         <div className="row">
           <h1 className="brand-text mb-5">My Skills Set</h1>
           <div className="col-md-8">
@@ -54,53 +33,53 @@ const Skills = () => {
           <div className="col-md-4">
             <div className="service-box">
               <h4 className="brand-text">Frontend skills: </h4>
-              {/* {frontendskills.map((skill) => (
+              {frontendskills.map((skill) => (
                 <span className="singleSkill">{skill}</span>
-              ))} */}
+              ))}
             </div>
           </div>
           <div className="col-md-4">
             <div className="service-box">
               <h4 className="brand-text">Backend skills: </h4>
-              {/* {backendskills.map((skill) => (
+              {backendskills.map((skill) => (
                 <span className="singleSkill">{skill}</span>
-              ))} */}
+              ))}
             </div>
           </div>
           <div className="col-md-4">
             <div className="service-box">
               <h4 className="brand-text">Database skills: </h4>
-              {/* {database.map((skill) => (
+              {database.map((skill) => (
                 <span className="singleSkill">{skill}</span>
-              ))} */}
+              ))}
             </div>
           </div>
           <div className="col-md-4">
             <div className="service-box1">
               <h4 className="brand-text">Programming Language: </h4>
-              {/* {programming.map((skill) => (
+              {programming.map((skill) => (
                 <span className="singleSkill">{skill}</span>
-              ))} */}
+              ))}
             </div>
           </div>
           <div className="col-md-4">
             <div className="service-box2">
               <h4 className="brand-text">Version Control: </h4>
-              {/* {version.map((skill) => (
+              {version.map((skill) => (
                 <span className="singleSkill">{skill}</span>
-              ))} */}
+              ))}
             </div>
           </div>
           <div className="col-md-4">
             <div className="service-box3">
               <h4 className="brand-text">Familiar with: </h4>
-              {/* {familiar.map((skill) => (
+              {familiar.map((skill) => (
                 <span className="lowskill">{skill}</span>
-              ))} */}
+              ))}
             </div>
           </div>
-          </div>
-          </div>
+        </div>
+      </div>
     </motion.div>
   );
 };
