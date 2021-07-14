ab65c0a7cb0e6881866775d14b0f2ec07d1a241c
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-07-14 18:05:36 +0300

    wip-technology-feature

--- my-portfolio/src/components/Projects/SingleProject.js
+++ my-portfolio/src/components/Projects/SingleProject.js
@@ -7,9 +7,9 @@ const SingleProject = ({project}) =>{
             <img className="w-100"src={img}alt=""/>
             <h5 className="mt-2">{title}</h5>
             <p>{description}</p>
-            {
+            {/* {
                 technology.map(tech => <span style={{fontsize:'10px'}}className="singleSkill">{tech}</span>)
-            }
+            } */}
             <div className="project-links h-100 position-absolute top-0 start-0 w-100">
                 <div className="h-100 d-flex justify-content-around align-items-center">
                     <a target="_blank"href={github}><button className="btn brand-btn">Github</button></a>
